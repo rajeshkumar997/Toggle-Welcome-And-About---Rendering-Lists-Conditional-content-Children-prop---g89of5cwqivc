e9148eab6f057affdbe21bce81bcb72bee9913a3
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-16 14:05:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,4 @@
-import React from "react";
-import "../styles/App.css";
-
-const App = () => {
-  return (import React,{useState} from "react";
+import React,{useState} from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -28,23 +24,6 @@ const App = () => {
       
       
       <button id="toggle" onClick={handleToggle}>Toggle</button>
-    </div>
-  );
-};
-
-export default App;
-    <div id="main">
-      <div id="welcome-div">
-        <h1>Welcome to Newton School!!</h1>
-      </div>
-      <div id="about-div">
-        <h1>
-          Newton School is a neo-university providing highly immersive tech
-          learning to millions of students, to tap into new-age tech
-          opportunities.
-        </h1>
-      </div>
-      <button id="toggle">Toggle</button>
     </div>
   );
 };
